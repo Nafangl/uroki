96317051b57e852a105976d48c370b34044979af
Dima <imsupstaR@mail.ru>
2020-04-08 10:10:39 +0700

    first commit
    first

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+
+let persone = { 
+    name: "john",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(persone['age']);
+
+//let arr = ['plum.png', 'orange.jpg', 'apple.png'];
+
+//console.log(arr[0])
+
+//alert('hello world!');
+
+//let answer = prompt('Есть ли вам 18?', 'Да');
+
+//console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+
+
+console.log(incr);
+console.log(decr);
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked || isClose);
