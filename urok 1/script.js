d9cc7da39ad1eb818c43b31291506558bbfa4ab7
Dima <imsupstaR@mail.ru>
2020-04-09 11:13:08 +0700

    урок

--- urok 1/script.js
+++ urok 1/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
@@ -14,22 +14,74 @@ let appData = {
     
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let a2 = prompt("Во сколько обойдется?", "");
-let a3 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let a4 = prompt("Во сколько обойдется?", "");
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+/*let i = 0;
+    while (i < 2) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+        let b = prompt("Во сколько обойдется?", "");
+        i++;
 
-alert(appData.budget / 30);
+        if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a !='' && b !='' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+    
+        }
+        
+
+        
+    }
+*/
+let i = 0;
+    do {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+        let b = prompt("Во сколько обойдется?", "");
+        i++;  
+
+        if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a !='' && b !='' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+    
+        }
+
+}
+while (i < 2);
 
 
 
-/*
-1. Типы данных в JS: числа, строки, буевеы(так и не понял что это за тип данных), специальные значения(null, undefined)
-символы и объекты
-2. Информацию в консоль выводят командый - console.log();
-3. Функции операторов: && и || - функции сверяют данные переменных */
+
+
+
+
+
+
+/*for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let b = prompt("Во сколько обойдется?", "");
+
+    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a !='' && b !='' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+
+    
+}
+*/
+appData.moneyPerDay = appData.budget / 30;
+
+alert("ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
 
 
